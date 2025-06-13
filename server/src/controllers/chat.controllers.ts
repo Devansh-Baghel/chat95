2b577307f46cfd5ea9ee3662b3e65acb9da251c3
Devansh Baghel <devanshbaghel85@gmail.com>
2025-06-14 03:42:52 +0530

    add chat model + controllers

---
+++ server/src/controllers/chat.controllers.ts
@@ -0,0 +1,57 @@
+import { ApiError } from "../utils/apiError";
+import { ApiResponse } from "../utils/apiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
+import { Chat } from "../models/chat.model";
+
+export const createNewChat = asyncHandler(async (req, res) => {
+  const { title, madeBy, question, answer } = req.body;
+
+  if (!madeBy) throw new ApiError(400, "Madeby is required");
+  if (!title) throw new ApiError(400, "Title is required");
+  if (!question) throw new ApiError(400, "Question is required");
+  if (!answer) throw new ApiError(400, "Answer is required");
+
+  const newChat = await Chat.create({
+    madeBy,
+    title,
+    messages: [{ question, answer }],
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, newChat, "New chat created sucessfully"));
+});
+
+export const addMessageToChat = asyncHandler(async (req, res) => {
+  const { chatId, question, answer } = req.body;
+
+  if (!chatId) throw new ApiError(400, "Chat id is required");
+  if (!question) throw new ApiError(400, "Question is required");
+  if (!answer) throw new ApiError(400, "Answer is required");
+
+  const chat = await Chat.findById(chatId);
+
+  if (!chat) throw new ApiError(404, "Chat not found");
+
+  chat.messages.push({ question, answer });
+
+  await chat.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, chat, "Message added to chat successfully"));
+});
+
+export const getChat = asyncHandler(async (req, res) => {
+  const { chatId } = req.body;
+
+  if (!chatId) throw new ApiError(400, "Chat id is required");
+
+  const chat = await Chat.findById(chatId);
+
+  if (!chat) throw new ApiError(404, "Chat not found");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, chat, "Chat found successfully"));
+});
---
+++ server/src/models/chat.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface Message {
+  question: string;
+  answer: string;
+  timestamp?: Date;
+}
+
+export interface ChatTypes extends Document {
+  _id: string;
+  madeBy: string;
+  title: string;
+  messages: Message[];
+}
+
+const chatSchema: Schema<ChatTypes> = new Schema(
+  {
+    madeBy: {
+      type: String,
+      required: [true, "Madeby is required"],
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+    },
+    messages: [
+      {
+        question: { type: String, required: true },
+        answer: { type: String, required: true },
+        timestamp: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Chat = mongoose.model<ChatTypes>("Chat", chatSchema);
